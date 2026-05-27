5a58fe05b5840d5726ee6ad1b32f9df4cb6dda49
TestUser <testuser@example.com>
2026-05-27 19:38:16 +0500

    Added new Customer Controller class

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using InventoryManagementSystemm.API.Data;
+using InventoryManagementSystemm.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagementSystemm.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CustomersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // 1. GET ALL
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            var customers = await _context.Customers.ToListAsync();
+            return Ok(customers);
+        }
+
+        // 2. GET BY ID
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        // 3. CREATE (POST)
+        [HttpPost]
+        public async Task<ActionResult<Customer>> CreateCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCustomer), new { id = customer.CustomerId }, customer);
+        }
+    }
+}
